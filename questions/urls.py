c72f34d1746779e1fcb8d23453526df20b77e64a
skylark9704 <vsushant97@gmail.com>
2020-02-21 16:40:53 +0530

    Add class based view

--- questions/urls.py
+++ questions/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import url
-from . import views
+from questions.views import QuestionsView, AnswerView, VoteView, QuestionView, TopicView, MyQuestionsView
 
 app_name = 'questions'
 urlpatterns = [
-    url(r'^ask/$', views.ask, name="ask"),
-    url(r'^topic/$', views.topic, name="topic"),
-    url(r'^topic/add$', views.topic, name="add_topic"),
-    url(r'^question/(?P<question_id>[0-9]+)/$', views.question, name="detailed"),
-    url(r'^vote/(?P<question_id>[0-9]+)$', views.vote, name="vote"),
-    url(r'^answer/(?P<question_id>[0-9]+)$', views.answer, name="answer"),
-    url(r'^mine/$', views.mine, name="mine"),
+    url(r'^$', QuestionsView.as_view(), name="ask"),
+    url(r'^topic/$', TopicView.as_view(), name="topic"),
+    url(r'^mine/$', MyQuestionsView.as_view(), name="mine"),
+    url(r'^question/(?P<question_id>[0-9]+)/$', QuestionView.as_view(), name="detailed"),
+    url(r'^vote/(?P<question_id>[0-9]+)$', VoteView.as_view(), name="vote"),
+    url(r'^answer/(?P<question_id>[0-9]+)$', AnswerView.as_view(), name="answer"),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,87 +1,93 @@
 from django.shortcuts import render, redirect
 from .models import Question, Vote, Answer, Topic
 from django.contrib.auth.decorators import permission_required
+from django.views import View
+
+
+class QuestionsView(View):
+    def get(self, request, question):
+        return render(request, 'questions/index.html')
+
+    def post(self, request):
+        topic = Topic.objects.get(pk=request.POST['topic'])
+        q = Question(
+            question_text=request.POST['text'],
+            title=request.POST['title'],
+            user=request.user,
+            topic=topic
+        )
+
+        q.save()
+        return redirect('/home/')
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'questions/index.html')
-
-
-def question(request, question_id):
-    query = Question.objects.get(pk=question_id)
-    question = {'question': query}
-    answers = Answer.objects.filter(question=query)
-    votes = Vote.objects.filter(question=query)
-    up = 0
-    down = 0
-    for vote in votes:
-        if vote.vote == 1:
-            up = up + 1
-        elif vote.vote == -1:
-            down = down + 1
-    return render(
-        request, 'questions/index.html',
-        {
-            'question': question,
-            'votes': {'up': up, 'down': down},
-            'answers': answers
-        }
-    )
-
-
-def vote(request, question_id):
-    q = Question.objects.get(pk=question_id)
-    v = get(Vote.objects.filter(user=request.user, question=q), 0)
-
-    if not v:
-        vote = Vote(
-            question=q,
-            vote=request.POST['vote'],
-            user=request.user
+class QuestionView(View):
+    def get(self, request, question_id):
+        query = Question.objects.get(pk=question_id)
+        question = {'question': query}
+        answers = Answer.objects.filter(question=query)
+        votes = Vote.objects.filter(question=query)
+        up = 0
+        down = 0
+        for vote in votes:
+            if vote.vote == 1:
+                up = up + 1
+            elif vote.vote == -1:
+                down = down + 1
+        return render(
+            request, 'questions/index.html',
+            {
+                'question': question,
+                'votes': {'up': up, 'down': down},
+                'answers': answers
+            }
         )
-        vote.save()
-
-    else:
-        if not f'{v.vote}' == request.POST['vote']:
-            v.vote = request.POST['vote']
-            v.save()
-
-    return redirect(f'/questions/question/{question_id}')
 
 
-def answer(request, question_id):
-    q = Question.objects.get(pk=question_id)
-    answer = Answer(
-        answer=request.POST['answer'],
-        user=request.user,
-        question=q
-    )
+class VoteView(View):
+    def get(self, request):
+        pass
 
-    answer.save()
-    return redirect(f'/questions/question/{question_id}')
+    def post(self, request, question_id):
+        q = Question.objects.get(pk=question_id)
+        v = get(Vote.objects.filter(user=request.user, question=q), 0)
+
+        if not v:
+            vote = Vote(
+                question=q,
+                vote=request.POST['vote'],
+                user=request.user
+            )
+            vote.save()
+
+        else:
+            if not f'{v.vote}' == request.POST['vote']:
+                v.vote = request.POST['vote']
+                v.save()
+
+        return redirect(f'/questions/question/{question_id}')
 
 
-def ask(request):
-    topic = Topic.objects.get(pk=request.POST['topic'])
-    q = Question(
-        question_text=request.POST['text'],
-        title=request.POST['title'],
-        user=request.user,
-        topic=topic
-    )
+class AnswerView(View):
+    def post(self, request, question_id):
+        q = Question.objects.get(pk=question_id)
+        answer = Answer(
+            answer=request.POST['answer'],
+            user=request.user,
+            question=q
+        )
 
-    q.save()
-    return redirect('/home/')
+        answer.save()
+        return redirect(f'/questions/question/{question_id}')
 
 
-@permission_required('topic.add_topic')
-def topic(request):
-    if(request.method == 'GET'):
+class TopicView(View):
+    def get(self, request):
         topics = Topic.objects.all()
         return render(request, 'questions/add_topics.html', {'topics': topics})
-    
-    elif(request.method == 'POST'):
+
+    def post(self, request):
         topic = Topic(
             topic=request.POST['topic']
         )
@@ -91,22 +97,23 @@ def topic(request):
         return redirect('/questions/topic')
 
 
-def mine(request):
-    questions = Question.objects.filter(user=request.user)
-    answers = Answer.objects.filter(user=request.user)
-    for row in questions:
-        up = 0
-        down = 0
-        votes = Vote.objects.filter(question=row)
-        for vote in votes:
-            if vote.vote == 1:
-                up = up + 1
-            elif vote.vote == -1:
-                down = down + 1
+class MyQuestionsView(View):
+    def get(self, request):
+        questions = Question.objects.filter(user=request.user)
+        answers = Answer.objects.filter(user=request.user)
+        for row in questions:
+            up = 0
+            down = 0
+            votes = Vote.objects.filter(question=row)
+            for vote in votes:
+                if vote.vote == 1:
+                    up = up + 1
+                elif vote.vote == -1:
+                    down = down + 1
 
-        row.up = up
-        row.down = down
-    return render(request, 'questions/my_questions.html', {'asked': questions, 'answered': answers})
+            row.up = up
+            row.down = down
+        return render(request, 'questions/my_questions.html', {'asked': questions, 'answered': answers})
 
 
 def get(l, index, default=False):
